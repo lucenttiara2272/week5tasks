2e28a00cf11a04c6079a61f34e716e7253dc0544
lucenttiara2272 <martinkiller323@hotmail.com>
2026-01-29 11:02:23 +0100

    Task 2 done:

--- a_test.mjs
+++ a_test.mjs
@@ -1,3 +1,4 @@
+
 import test from "./test.mjs";
 
 function sum (a,b){
@@ -10,3 +11,26 @@ sumTester.isEqual(sum(99,1),99,"99*1 is equal to 99");
 sumTester.isEqual(sum(0,-1),0,"0*1 is equal to 0");
 sumTester.isEqual(sum(-99,-1),99,"-99*1 is equal to -99");
 sumTester.isEqual(sum(-10,1),-10,"-10*1 is equal to -10");
+
+function formatName(firstName,lastName) {
+
+    let firstNameLowercase;
+    let lastNameLowercase;
+    let firstNameRemoveSpaces;
+    let lastNameRemoveSpaces;
+
+    firstNameLowercase = firstName.toLowerCase(firstName);
+    lastNameLowercase = lastName.toLowerCase(lastName);
+
+    firstNameRemoveSpaces = firstNameLowercase.trim(firstNameLowercase);
+    lastNameRemoveSpaces = lastNameLowercase.trim(lastNameLowercase);
+
+    let combinedName = firstNameRemoveSpaces + " " + lastNameRemoveSpaces;
+
+    let output = combinedName
+    output.toString();
+    output.toLowerCase();
+    return output;
+}
+
+console.log(formatName("    Martin   ","   BJØRKLUND   "));
